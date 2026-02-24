ca8df1979a1b447d5d9a52b0f0f1670484b08c7d
Lollosmino42 <arc042r@proton.me>
2026-02-25 00:02:55 +0100

    Initial commit

--- Autobuild.java
+++ Autobuild.java
@@ -75,7 +75,6 @@ public final class Autobuild {
 
 		build(buildFiles, flags);
 		execute(mainClass);
-
 	}
 
 
--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
-import more.More;
-
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello Fish!");
-		More.sayHi();
+		System.out.println("Hello Trash!");
 	}
 }
